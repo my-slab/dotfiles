aaa40374a07f32c05d36326343c0fe788c9c4a9b
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-05-15 10:40:41 +1000

    Create cask.sh

---
+++ cask.sh
@@ -0,0 +1,17 @@
+brew cask install boom
+
+brew cask install datagrip
+
+brew cask install docker
+
+brew cask install google-chrome
+
+brew cask install iterm
+
+brew cask install slack
+
+brew cask install spotify
+
+brew cask install telegram
+
+brew cask install visual-studio-code
