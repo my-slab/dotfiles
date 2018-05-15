ec4ec6ceedb5f90ac2f617688ef20c76bfc89c80
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-05-15 10:43:37 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,3 @@
-# Brew
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Casks
-
-brew cask install boom
-
-brew cask install datagrip
-
-brew cask install docker
-
-brew cask install google-chrome
-
-brew cask install iterm
-
-brew cask install slack
-
-brew cask install spotify
-
-brew cask install telegram
-
-brew cask install visual-studio-code
-
-# Keys
-
-ssh-keygen -t rsa -b 4096 -C "mitchell.stewart@mylsb.email"
-
-eval "$(ssh-agent -s)"
-
-echo "Host *
- AddKeysToAgent yes
- UseKeychain yes
- IdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
-
-ssh-add -K ~/.ssh/id_rsaMitchells-MacBook-Pro:~ slab$
+./brew.sh
+./cask.sh
+./keys.sh
