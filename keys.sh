f6fbb26cb24e34dfa1ced39d5b3bd916685a0f6d
Mitch Stewart <mitch.stewart@hey.com>
2022-06-02 11:09:16 +1000

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -2,9 +2,11 @@ ssh-keygen -t ed25519 -C "mitch.stewart@hey.com"
 
 eval "$(ssh-agent -s)"
 
+touch ~/.ssh/conig
+
 echo "Host *
  AddKeysToAgent yes
  UseKeychain yes
  IdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
 
-ssh-add -K ~/.ssh/id_rsa
+ssh-add -K ~/.ssh/id_ed25519
